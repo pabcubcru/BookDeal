d7528faa4cde650758f30c0a243b7ad7d1977e1d
pabcubcru <pabcubcru@alum.us.es>
2021-06-15 18:44:02 +0200

    Finalizados test de modelo.

--- src/test/java/com/pabcubcru/bookdeal/model/UserValidatorTests.java
+++ src/test/java/com/pabcubcru/bookdeal/model/UserValidatorTests.java
@@ -1,7 +1,6 @@
 package com.pabcubcru.bookdeal.model;
 
 import java.time.LocalDate;
-import java.util.Locale;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -10,11 +9,8 @@ import javax.validation.Validator;
 import com.pabcubcru.bookdeal.models.User;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 public class UserValidatorTests {
@@ -26,12 +22,25 @@ public class UserValidatorTests {
         return validator;
     }
 
-    /*@ParameterizedTest
+    @ParameterizedTest
     @CsvFileSource(resources = "../csv/model/Users.csv", encoding = "utf-8", numLinesToSkip = 1, delimiterString = ";")
-    void errorWhenFieldIsNullOrBlankOrNotValid() throws Exception {
+    void errorWhenFieldIsNullOrBlankOrNotValid(String name, String email, String phone, LocalDate birthDate,
+            String province, String postCode, String genres, String username, String password, Boolean enabled,
+            Boolean accept, String confirmPassword) throws Exception {
+
+        User user = new User(name, email, phone, birthDate, province, postCode, genres, username, password, enabled,
+                accept, confirmPassword);
 
         Validator validator = createValidator();
         Set<ConstraintViolation<User>> constrains = validator.validate(user);
+        if(constrains.size() > 1) {
+            System.out.println(constrains);
+            System.out.println(user.getUsername());
+        }
+
+        if(constrains.size() < 1) {
+            System.out.println("----------------" + user.getUsername());
+        }
         Assertions.assertThat(constrains.size()).isEqualTo(1);
-    }*/
+    }
 }
